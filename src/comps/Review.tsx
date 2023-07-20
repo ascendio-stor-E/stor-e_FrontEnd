f2d5c8338596cc3a6ec48e034a03571986c588c5
Robbie-Corcoran <codewithrobbie@gmail.com>
2023-07-20 21:10:49 +0200

    Inital set up

--- src/comps/Review.tsx
+++ src/comps/Review.tsx
@@ -1,12 +1,36 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
+import Carousel from 'react-bootstrap/Carousel';
 
 const Review = () => {
   return (
-    <div>
-      Review
-    </div>
-  )
-}
+    <section>
+      <Carousel interval={null}>
+        <Carousel.Item>
+          <img className="home__logo" src="./src/assets/Ascendio Logo.png" alt="Dummy Ascendio Logo" />
+          <p className="review__storyText">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur cupiditate aliquid incidunt vero rem earum facilis doloribus nisi voluptatum repudiandae.</p>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="home__logo" src="./src/assets/Ascendio Logo.png" alt="Dummy Ascendio Logo" />
+          <p className="review__storyText">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur cupiditate aliquid incidunt vero rem earum facilis doloribus nisi voluptatum repudiandae.</p>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="home__logo" src="./src/assets/Ascendio Logo.png" alt="Dummy Ascendio Logo" />
+          <p className="review__storyText">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur cupiditate aliquid incidunt vero rem earum facilis doloribus nisi voluptatum repudiandae.</p>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="home__logo" src="./src/assets/Ascendio Logo.png" alt="Dummy Ascendio Logo" />
+          <p className="review__storyText">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur cupiditate aliquid incidunt vero rem earum facilis doloribus nisi voluptatum repudiandae.</p>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="home__logo" src="./src/assets/Ascendio Logo.png" alt="Dummy Ascendio Logo" />
+          <p className="review__storyText">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur cupiditate aliquid incidunt vero rem earum facilis doloribus nisi voluptatum repudiandae.</p>
+        </Carousel.Item>
+      </Carousel>
+      <button>Delete</button>
+      <button>Save</button>
+    </section>
+  );
+};
 
-export default Review
+export default Review;
