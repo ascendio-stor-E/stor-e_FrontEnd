99bb114288b5e05c56894b333d4af3949966a0e8
Robbie-Corcoran <codewithrobbie@gmail.com>
2023-07-21 11:04:52 +0200

    Displaying story text

--- src/comps/Review.tsx
+++ src/comps/Review.tsx
@@ -2,30 +2,31 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import Carousel from 'react-bootstrap/Carousel';
 import { useNavigate, useParams } from 'react-router-dom';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
-import { StoryPageType } from '../types/StoryPageType';
+import { StoryPageData } from '../types/StoryPageData';
 
-type StoryBookProps = {
-  currentStoryBook: StoryPageType | undefined;
+type StoryPageData = {
+  id: string;
+  textContent: string;
+  pageNumber: number;
+  image: string;
 };
 
 const Review = (props: StoryBookProps) => {
   const { storyBookId } = useParams<{ storyBookId: string }>();
   const navigate = useNavigate();
+  const [storyPages, setStoryPages] = useState<StoryPageData[]>([]);
+  const [imageData, setImageData] = useState<{ [key: string]: string }>({});
   console.log(storyBookId);
 
   useEffect(() => {
     const getData = () => {
       axios
-        .get(`http://localhost:8080/api/story/all/${storyBookId}`)
+        .get(`https://stor-e.purplesea-320b619b.westeurope.azurecontainerapps.io/api/story/all/${storyBookId}`)
         .then((response) => {
           console.log('Got response', response.data);
-          // const storyData: StoryPageType = {
-          //   part: response.data.pageNumber;
-          //   story: response.data.textContext;
-          //   image: response.data.image;
-          // }
+          setStoryPages(response.data);
         })
         .catch((err) => console.error('Cannot review story', err));
     };
@@ -46,26 +47,12 @@ const Review = (props: StoryBookProps) => {
   return (
     <section>
       <Carousel interval={null}>
-        <Carousel.Item>
-          <img className="home__logo" src="../src/assets/Ascendio Logo.png" alt="Dummy Ascendio Logo" />
-          <p className="review__storyText">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsa alias ex adipisci esse sapiente ad commodi quis doloribus officiis repudiandae.</p>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img className="home__logo" src="../src/assets/Ascendio Logo.png" alt="Dummy Ascendio Logo" />
-          <p className="review__storyText">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsa alias ex adipisci esse sapiente ad commodi quis doloribus officiis repudiandae.</p>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img className="home__logo" src="../src/assets/Ascendio Logo.png" alt="Dummy Ascendio Logo" />
-          <p className="review__storyText">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsa alias ex adipisci esse sapiente ad commodi quis doloribus officiis repudiandae.</p>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img className="home__logo" src="../src/assets/Ascendio Logo.png" alt="Dummy Ascendio Logo" />
-          <p className="review__storyText">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsa alias ex adipisci esse sapiente ad commodi quis doloribus officiis repudiandae.</p>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img className="home__logo" src="../src/assets/Ascendio Logo.png" alt="Dummy Ascendio Logo" />
-          <p className="review__storyText">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsa alias ex adipisci esse sapiente ad commodi quis doloribus officiis repudiandae.</p>
-        </Carousel.Item>
+        {storyPages.map((page) => (
+          <Carousel.Item key={page.id}>
+            <img className="home__logo" src={`../src/assets/Ascendio Logo.png`} alt="Dummy Ascendio Logo" />
+            <p className="review__storyText">{page.textContent}</p>
+          </Carousel.Item>
+        ))}
       </Carousel>
       <button onClick={handleDeleteClick}>Delete</button>
       <button onClick={handleSaveClick}>Save</button>
