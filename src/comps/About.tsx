8aaa86b1259146b73497e15cf8831c104ad52ba9
Robbie-Corcoran <codewithrobbie@gmail.com>
2023-07-22 17:02:38 +0200

    Removed loading from About

--- src/comps/About.tsx
+++ src/comps/About.tsx
@@ -1,10 +1,7 @@
-import Loading from "./loadingComp/Loading";
-
 const About = () => {
     return (
       <section className="about">
         <h1>About us</h1>
-        <Loading />
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam aut consectetur provident est dignissimos eius facere officia, voluptatibus temporibus at molestias, explicabo suscipit nulla ratione nostrum ipsa quas eligendi possimus.</p>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur sed nemo illo nihil praesentium? Officia porro fugit deserunt tempore, aperiam quidem quibusdam sit repellat pariatur eligendi, eveniet qui eos autem alias beatae corrupti expedita cumque eum maxime quaerat! Sequi maiores molestiae, dolores fugiat tenetur, eligendi doloribus veniam iste recusandae, temporibus dolor sit aperiam distinctio soluta dolorem facere modi qui nulla nisi mollitia ipsam libero asperiores ipsa provident. Accusantium facilis a doloribus maiores optio officia dolores doloremque! Aperiam quod, iure expedita vitae ea quasi maiores excepturi optio debitis sequi laudantium atque rem repellendus, odit beatae velit explicabo rerum. In iste quia recusandae aut eos omnis temporibus excepturi labore voluptates aspernatur, explicabo voluptatem sunt adipisci rem ab cumque molestias. Cum eligendi totam, repellat animi vero sunt aliquam architecto earum corrupti et nesciunt non cupiditate officiis doloremque nihil saepe ex facere ipsam asperiores enim necessitatibus voluptate temporibus. Ad, necessitatibus iure. Itaque quas dolor accusantium, consectetur aliquam, atque sequi dolores autem, temporibus iusto repellendus dignissimos. Laboriosam, earum incidunt! Optio natus dignissimos, quia voluptas inventore rerum iusto sunt quibusdam ex! Hic blanditiis, numquam dolores quisquam maxime accusamus exercitationem quis esse quasi, rerum doloremque iure delectus eos inventore error, assumenda mollitia dolorum autem? Placeat, debitis id!</p>
       </section>
