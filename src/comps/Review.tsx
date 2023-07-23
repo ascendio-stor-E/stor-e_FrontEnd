415be68076a89f43ea30abe689c7d1e8751188dc
Robbie-Corcoran <codewithrobbie@gmail.com>
2023-07-23 21:22:31 +0200

    Review updates

--- src/comps/Review.tsx
+++ src/comps/Review.tsx
@@ -8,9 +8,11 @@ import { StoryPageData } from '../types/StoryPageData';
 
 const Review = () => {
   const { storyBookId } = useParams<{ storyBookId: string }>();
+  const [ storyPages, setStoryPages ] = useState<StoryPageData[]>([]);
+  const [ storyTitle, setStoryTitle ] = useState<string>("");
+  const [ pageNumber, setPageNumber ] = useState<number>(1);
   const navigate = useNavigate();
-  const [storyPages, setStoryPages] = useState<StoryPageData[]>([]);
-  console.log(storyBookId);
+  // console.log(storyBookId);
 
   useEffect(() => {
     const getData = () => {
@@ -20,11 +22,23 @@ const Review = () => {
           console.log('Got response', response.data);
           const sortedPages = response.data.sort((a: { pageNumber: number }, b: { pageNumber: number }) => a.pageNumber - b.pageNumber);
           setStoryPages(sortedPages);
+          setPageNumber(sortedPages[0]?.pageNumber || 1);
         })
         .catch((err) => console.error('Cannot review story', err));
     };
     getData();
-  }, [storyBookId]);
+
+    const getTitle = () => {
+      axios
+      .get(`https://stor-e.purplesea-320b619b.westeurope.azurecontainerapps.io/api/storybook/${storyBookId}`)
+      .then((response) => {
+        setStoryTitle(response.data.title)
+      })
+      .catch((err) => console.error('Cannot review story', err));
+  };
+  getTitle();
+
+    }, [storyBookId]);
 
   const handleDeleteClick = () => {
     axios.delete(`https://stor-e.purplesea-320b619b.westeurope.azurecontainerapps.io/api/storybook/${storyBookId}`).catch((err) => console.error('Cannot delete story', err));
@@ -37,16 +51,23 @@ const Review = () => {
 
   return (
     <section>
+      <h3>{storyTitle}</h3>
       <Carousel
         interval={null}
         indicators={false}
-        prevIcon={<img className="review__carousel--arrow" src="../src/assets/arrowLeft.png" alt="Prev" />} // Use your local custom prev arrow image
-        nextIcon={<img className="review__carousel--arrow" src="../src/assets/arrowRight.png" alt="Next" />} // Use your local custom next arrow image
-      >
+        prevIcon={pageNumber === 1 ? null : <img className="review__carousel--arrow" src="../src/assets/arrowLeft.png" alt="Prev" />}
+        nextIcon={pageNumber === storyPages.length ? null : <img className="review__carousel--arrow" src="../src/assets/arrowRight.png" alt="Next" />}
+        onSelect={(selectedIndex) => {
+          setPageNumber(storyPages[selectedIndex]?.pageNumber || 1);
+        }}
+        
+     >
         {storyPages.map((page) => (
           <Carousel.Item key={page.id}>
             <img className="home__logo" src={`https://stor-e.purplesea-320b619b.westeurope.azurecontainerapps.io/api/story/image/${page.image}`} alt="Story Image" />
-            <p className="review__storyText">{page.textContent}</p>
+            <p className="review__storyText">{page.textContent}
+              <br /><span className='review__storyText--pageNumber'>Page {pageNumber}</span>
+            </p>
           </Carousel.Item>
         ))}
       </Carousel>
