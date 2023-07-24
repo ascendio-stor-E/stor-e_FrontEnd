0940683107850f8a8318ade080cec8822f4ccbe3
Robbie-Corcoran <codewithrobbie@gmail.com>
2023-07-24 12:28:05 +0200

    Frontend tweaks

--- src/comps/loadingComps/Loading.tsx
+++ src/comps/loadingComps/Loading.tsx
@@ -8,7 +8,7 @@ const Loading = () => {
   useEffect(() => {
     const loadingInterval = setInterval(() => {
       setLoadingIndex((prevIndex) => Math.floor(Math.random() * loadingText.length));
-    }, 2000);
+    }, 4000);
     return () => {
       clearInterval(loadingInterval);
     };
