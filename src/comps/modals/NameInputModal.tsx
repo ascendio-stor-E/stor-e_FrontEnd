864bd4fb32753184871eb44819f8434a22aa222f
Robbie-Corcoran <codewithrobbie@gmail.com>
2023-07-26 12:48:51 +0200

    Modals styled, colour decisions needed

--- src/comps/modals/NameInputModal.tsx
+++ src/comps/modals/NameInputModal.tsx
@@ -1,15 +1,15 @@
 import { Modal, Button } from 'react-bootstrap';
-import { ModalProps } from '../../types/ModalProps'
+import { ModalProps } from '../../types/ModalProps';
 
 const NameInputModal = (props: ModalProps) => {
   return (
-    <Modal show={props.show} onHide={props.onClose}>
+    <Modal show={props.show} onHide={props.onClose} aria-labelledby="contained-modal-title-vcenter" centered>
       <Modal.Header closeButton>
         <Modal.Title>Missing Character Name</Modal.Title>
       </Modal.Header>
       <Modal.Body>Please enter a name for your character before proceeding.</Modal.Body>
       <Modal.Footer>
-        <Button variant="secondary" onClick={props.onClose}>
+        <Button className="modal__button" variant="primary" onClick={props.onClose}>
           Close
         </Button>
       </Modal.Footer>
