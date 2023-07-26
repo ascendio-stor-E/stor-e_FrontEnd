2c2ac242e1e2f2423dac07552b18bd1747248a94
Robbie-Corcoran <codewithrobbie@gmail.com>
2023-07-26 06:45:20 +0200

    Team member cards added

--- src/comps/About.tsx
+++ src/comps/About.tsx
@@ -1,15 +1,58 @@
-import { stopNarration } from "../common/helpers/VoiceNarrator";
-import logo from "./../assets/Store-E_Logo_V2.png";
+import logo from './../assets/Store-E_Logo_V2.png';
+import AysePhoto from '../assets/Ayse.png';
+import RobbiePhoto from '../assets/Robbie.png';
+import VijaniPhoto from '../assets/Vijani.png';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
 const About = () => {
-    stopNarration();
-    return (
-      <section className="about">
-        <h3>Introduction to Store-E</h3>
-        <h3>Development Team</h3>
-        <img className="home__logo" src={logo} alt="Stor-E Logo" />
-      </section>
-    )
-  };
-  
-  export default About;
+  return (
+    <>
+      <img className="home__logo" src={logo} alt="Stor-E Logo" />
+    <section className="about">
+        <h1 className="about__title">Meet The Team</h1>
+        <div className="about__divider"></div>
+        <div className="about__members">
+          <div className="about__member">
+            <img className='about__image' width={200} height={200} src={AysePhoto} />
+            <div className="about__description">
+              <h1 className='about_name'>Ayse Akir</h1>
+              <h2>Developer</h2>
+              <p>Consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat. Lorem ipsum dolor sit amet consectet.</p>
+              <div className="about__social-media">
+                <LinkedInIcon />
+                <GitHubIcon />
+              </div>
+            </div>
+          </div>
+          <div className="about__member">
+            <img className='about__image' width={200} height={200} src={RobbiePhoto} />
+            <div className="about__description">
+              <h1 className='about_name'>Robbie Corcoran</h1>
+              <h2>Developer</h2>
+              <p>Consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat. Lorem ipsum dolor sit amet consectet.</p>
+              <div className="about__social-media">
+                <LinkedInIcon />
+                <GitHubIcon />
+              </div>
+            </div>
+          </div>
+          <div className="about__member">
+            <img className='about__image' width={200} height={200} src={VijaniPhoto} />
+            <div className="about__description">
+              <h1 className='about_name'>Vijani Piyawardana</h1>
+              <h2>Developer</h2>
+              <p>Consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat. Lorem ipsum dolor sit amet consectet.</p>
+              <div className="about__social-media">
+                <LinkedInIcon />
+                <GitHubIcon />
+              </div>
+            </div>
+          </div>
+        </div>
+    </section>
+    </>
+  );
+};
+
+export default About;
